866a9bb0d5c51ef524e2dc3c3748751bddbfe486
umiiali4433@gmail.com <Umairali100>
2020-08-31 16:29:20 +0500

    done done

--- lib/Views/Exercise.dart
+++ lib/Views/Exercise.dart
@@ -1,6 +1,7 @@
 import 'dart:core';
 
 import 'package:aman_app/Views/Login.dart';
+import 'package:aman_app/model/chotewalaadeel.dart';
 import 'package:aman_app/model/getlink.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,14 +14,16 @@ class Exercise extends StatefulWidget {
   sub createState() => sub();
 }
 class sub extends State<Exercise>{
-  List<getlink> getalldatalist = [];
+  List<loru> getalldatalist = [];
 
   @override
   void initState() {
     List<String> get1 = ['Harbour Phase','Sea Phase'];
     List<String> get2 = ['Opening Ceremony','Reception Dinner','IMC','Band Display & CT Demo','Food Gala & cultural show','IFR(internal fleet review)'];
     List<String> get3 = ['Events','Date & time'];
-   getalldatalist = [new getlink.a2('Activities',get1),new getlink.a2('Events',get2),new getlink.a2('Scedule',get3)];
+   getalldatalist.add(loru('Activities',get1));
+    getalldatalist.add(loru('Events',get2));
+    getalldatalist.add(loru('Scedule',get3));
   }
 
 
@@ -31,71 +34,81 @@ class sub extends State<Exercise>{
   final Color overlay = Color(0xff212129);
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return Scaffold(
+      body: ListView.builder(
 
-      shrinkWrap: true,
-      itemCount: getalldatalist.length,
-      itemBuilder: (ctx,int){
-        return Container(
-          child: Column(
-            children: [
-              Container(
-                height:MediaQuery.of(context).size.height*.3,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/background.jpeg'),
-                        //radius kese dete the?
-                        fit: BoxFit.cover)
+        shrinkWrap: true,
+        itemCount: getalldatalist.length,
+        itemBuilder: (ctx,int){
+          return Container(
+            margin: EdgeInsets.all(4),
+            child: Column(
+              children: [
+                Container(
+                  height:MediaQuery.of(context).size.height*.3,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/background.jpeg'),
+                          //radius kese dete the?
+                          fit: BoxFit.cover)
 
-                ),
-                child: Column(
+                  ),
+                  child: Column(
 
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(getalldatalist[int].key+" "+getalldatalist[int].getlinklist[int]),
-                    Container(
-                      height: 60.0,
-                      child: ListView.builder(
-                          shrinkWrap: true,
-                          scrollDirection: Axis.horizontal,
-                          itemCount: 15,
-                          itemBuilder: (BuildContext context, int) => Container(
-                            margin: EdgeInsets.only(bottom: 10),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(30.0),
-                                color: Theme.of(context).primaryColor),
-                            child: Padding(
-                              padding: EdgeInsets.all(9),
-                              child: Text(
-                                "gotthis on",
-                                style: TextStyle(color: Colors.white,fontSize: 25),
-                              ),
-                            ),
-                          )
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+
+                          color: Colors.black54,
+
+                          child: Text(getalldatalist[int].key,style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),)),
+                      SizedBox(
+                        height: 50,
                       ),
-                    ),
+                      Container(
+                        height: 50.0,
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: getalldatalist[int].getlinklist.length,
+                            itemBuilder: (BuildContext context, int2) => Container(
+                              margin: EdgeInsets.only(bottom: 10),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30.0),
+                                  color: Theme.of(context).primaryColor),
+                              child: Padding(
+                                padding: EdgeInsets.all(9),
+                                child: Text(
+                                  getalldatalist[int].getlinklist[int2],
+                                  style: TextStyle(color: Colors.white,fontSize: 15),
+                                ),
+                              ),
+                            )
+                        ),
+                      ),
 
 
-                  ],
-                ),
+                    ],
+                  ),
 
-                //setha??
+                  //setha??
 
-                //ub sun yr decoration nh chl rhee pta nh q is lis main ruk dikata hn
+                  //ub sun yr decoration nh chl rhee pta nh q is lis main ruk dikata hn
 //                 child: Image.asset('assets/images/background.jpeg',fit: BoxFit.cover),
 
-              ),
+                ),
 
-              SizedBox(
-                height: 20,
-              )
-            ],
-          ),
-        );
-      },
+                SizedBox(
+                  height: 5,
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
 
   }
--- lib/model/getlink.dart
+++ lib/model/getlink.dart
@@ -9,8 +9,15 @@ class getlink{
   List<String> getlinklist;
 ///asdadad
   ///dasdasda
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
   getlink.a1(this.key,this.test);
   getlink(this.key,this.value);
+
   getlink.a2(this.key,getlinklist);
   List<String> getit(){
     return getlinklist;
